82c41fdb10b7d5ce6400547a32cd322f4c746b02
Alexandre Beauvois <abeauvoi@student.42.fr>
2020-04-28 19:20:09 +0100

    unfinished

---
+++ bonus0/source.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+void	p(char *s1, char *s2) {
+	char 	buf[4096];
+
+	puts(s2);
+	read(0, buf, sizeof(buf));
+	*(strchr(buf, '\n')) = '\0';
+	strncpy(s1, buf, 0x14);
+}
+
+void 	pp(char *buf0) {
+	char 	buf1[0x14];
+	char 	buf2[0x14];
+
+	p(buf1, " - ");
+	p(buf2, " - ");
+	strcpy(buf0, buf1);
+	strcat(buf0 + strlen(buf0), buf2);
+}
+
+int		main(int ac, char *av[]) {
+	char 	buf[];
+
+	pp(buf);
+	puts(buf);
+	return 0;
+}
